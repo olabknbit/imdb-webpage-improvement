86411b85a495baba0427d06097b3b3cd9a560ddd
Aleksandra <al.byczynska@gmail.com>
2019-12-11 18:45:42 +0100

    stranger things

--- send_sparql_query.py
+++ send_sparql_query.py
@@ -3,8 +3,16 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 sparql = SPARQLWrapper("http://dbpedia.org/sparql")
 sparql.setQuery("""
     PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    SELECT ?label
-    WHERE { <http://dbpedia.org/resource/Asturias> rdfs:label ?label }
+    PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
+
+    SELECT distinct ?film ?label where  {
+    { ?film a dbpedia-owl:TelevisionShow }
+    ?film rdfs:label ?label .
+    FILTER regex( str(?label), "^stranger things"@en, "i") .
+    FILTER langMatches(lang(?label), "en")
+}
+
+limit 1000
 """)
 sparql.setReturnFormat(JSON)
 results = sparql.query().convert()
