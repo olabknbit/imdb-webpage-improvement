f14df830162b92382e37c776bf42156680115488
Aleksandra Bułka <aleksandra.bulka@codility.com>
2019-12-11 19:59:39 +0100

    update

--- send_sparql_query.py
+++ send_sparql_query.py
@@ -2,20 +2,23 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 
 sparql = SPARQLWrapper("http://dbpedia.org/sparql")
 sparql.setQuery("""
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>
+    #
+prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+prefix dbpedia-owl: <http://dbpedia.org/ontology/>
 
-    SELECT distinct ?film ?label where  {
-    { ?film a dbpedia-owl:TelevisionShow }
-    ?film rdfs:label ?label .
-    FILTER regex( str(?label), "^stranger things"@en, "i") .
-    FILTER langMatches(lang(?label), "en")
+select ?net where  {
+  { ?film a dbpedia-owl:TelevisionShow }
+  ?film rdfs:label ?label .
+  ?film dbo:network ?net
+  filter regex( str(?label), "^stranger things", "i") .
+FILTER langMatches(lang(?label), "en")
 }
 
-limit 1000
+limit 1
+
 """)
 sparql.setReturnFormat(JSON)
 results = sparql.query().convert()
 
 for result in results["results"]["bindings"]:
-    print('%s: %s' % (result["label"]["xml:lang"], result["label"]["value"]))
+    print('%s' % (result["net"]["value"]))
