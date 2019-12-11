9abe20a819af2d7b2714f37f816279c2fcf88e8f
Aleksandra <al.byczynska@gmail.com>
2019-12-11 20:05:24 +0100

    working lololo

--- send_sparql_query.py
+++ send_sparql_query.py
@@ -1,24 +1,29 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 
-sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-sparql.setQuery("""
-    #
-prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-prefix dbpedia-owl: <http://dbpedia.org/ontology/>
 
-select ?net where  {
-  { ?film a dbpedia-owl:TelevisionShow }
-  ?film rdfs:label ?label .
-  ?film dbo:network ?net
-  filter regex( str(?label), "^stranger things", "i") .
-FILTER langMatches(lang(?label), "en")
-}
+def get_network_name():
+    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    sparql.setQuery("""
+        prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        prefix dbpedia-owl: <http://dbpedia.org/ontology/>
 
-limit 1
+        select ?net where  {
+        { ?film a dbpedia-owl:TelevisionShow }
+        ?film rdfs:label ?label .
+        ?film dbo:network ?net
+        filter regex( str(?label), "^stranger things", "i") .
+        FILTER langMatches(lang(?label), "en")
+        }
 
-""")
-sparql.setReturnFormat(JSON)
-results = sparql.query().convert()
+        limit 1
+    """)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
 
-for result in results["results"]["bindings"]:
-    print('%s' % (result["net"]["value"]))
+    for result in results["results"]["bindings"]:
+        return result['net']['value']
+        # print('%s: %s' % (result["label"]["xml:lang"], result["label"]["value"]))
+
+
+if __name__ == "__main__":
+    get_network_name()
