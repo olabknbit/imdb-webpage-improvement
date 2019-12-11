6d3a74c963c6bcfdbe9542be2b060d0876da6f73
Aleksandra <al.byczynska@gmail.com>
2019-12-11 17:57:28 +0100

    extruct

--- web_scrapper.py
+++ web_scrapper.py
@@ -1,20 +1,28 @@
 import requests
 from bs4 import BeautifulSoup as bs
-
-url = 'https://www.imdb.com/title/tt6257970/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=332cb927-0342-42b3-815c-f9124e84021d&pf_rd_r=0DRP9V1ZZYC33XPZKM1T&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=tvmeter&ref_=chttvm_tt_7'
-response = requests.get(url)
-
-soup = bs(response.text, 'html.parser')
-
-# tags = ['style', 'script', 'head', 'title', '[document]']
-# for t in tags:
-#     [s.extract() for s in soup(t)]
-for x in soup.find_all("meta"):
-    print(x)
-
-
 #
-# for el in soup.find_all()[2:]:
-#     text_el = el.text.strip()
-#     print(el.name, '\t', el.attrs, '\t', text_el)
-# # print(response.text)
+# url = 'https://www.imdb.com/title/tt6257970/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=332cb927-0342-42b3-815c-f9124e84021d&pf_rd_r=0DRP9V1ZZYC33XPZKM1T&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=tvmeter&ref_=chttvm_tt_7'
+# response = requests.get(url)
+#
+# soup = bs(response.text, 'html.parser')
+#
+# # tags = ['style', 'script', 'head', 'title', '[document]']
+# # for t in tags:
+# #     [s.extract() for s in soup(t)]
+# for i, x in enumerate(soup.find_all("meta")[6:]):
+#     print(i, x)
+
+
+import extruct
+import requests
+import pprint
+from w3lib.html import get_base_url
+
+pp = pprint.PrettyPrinter(indent=2)
+url = 'https://www.imdb.com/title/tt6257970/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=332cb927-0342-42b3-815c-f9124e84021d&pf_rd_r=0DRP9V1ZZYC33XPZKM1T&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=tvmeter&ref_=chttvm_tt_7'
+
+r = requests.get(url)
+base_url = get_base_url(r.text, r.url)
+data = extruct.extract(r.text, base_url=base_url)
+
+pp.pprint(data)
