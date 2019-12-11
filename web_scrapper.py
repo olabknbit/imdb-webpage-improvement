014d688ba72830dd21ac602943e85d8bf53df118
Aleksandra <al.byczynska@gmail.com>
2019-12-11 18:44:13 +0100

    send sparql queries

--- web_scrapper.py
+++ web_scrapper.py
@@ -19,8 +19,7 @@ import pprint
 from w3lib.html import get_base_url
 
 pp = pprint.PrettyPrinter(indent=2)
-url = 'https://www.imdb.com/title/tt6257970/?pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=332cb927-0342-42b3-815c-f9124e84021d&pf_rd_r=0DRP9V1ZZYC33XPZKM1T&pf_rd_s=center-1&pf_rd_t=15506&pf_rd_i=tvmeter&ref_=chttvm_tt_7'
-
+url = 'https://www.imdb.com/title/tt4574334/?ref_=fn_al_tt_1'
 r = requests.get(url)
 base_url = get_base_url(r.text, r.url)
 data = extruct.extract(r.text, base_url=base_url)
