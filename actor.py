820c300f0725027309d1117fcae40492a168f006
Aleksandra <al.byczynska@gmail.com>
2020-01-20 18:21:42 +0100

    make actors show nicely

--- actor.py
+++ actor.py
@@ -2,11 +2,17 @@ from typing import Dict
 
 
 class Actor:
-    def __init__(self, uri: str, name: str, handles: Dict[str, str], date_of_birth: str = None):
-        self.uri = uri
+    def __init__(self, dbpedia_uri: str = None, wikidata_uri: str = None, name: str = None,
+                 handles: Dict[str, str] = None, date_of_birth: str = None):
+        self.wikidata_uri = wikidata_uri
+        self.dbpedia_uri = dbpedia_uri
         self.name = name
         self.date_of_birth = date_of_birth
         self.handles: Dict[str, str] = handles
 
     def to_string(self):
-        return self.name + " was born " + self.date_of_birth if self.date_of_birth else self.name
+        return str(self.name) + " was born " + self.date_of_birth if self.date_of_birth else self.name
+
+    def get_uri(self):
+        uri = ("http://www.wikidata.org/entity/" + self.wikidata_uri) if self.wikidata_uri is not None else ""
+        return uri
